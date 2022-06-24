c4153f79d0f93cae810bb47e83070b19bed527fc
maryuandi <maryuandidevrizal@gmail.com>
2022-06-24 13:09:39 +0700

    third commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as AuthUser;
 
-class User extends Authenticatable
+class User extends AuthUser
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $table = 'tb_user';
+    protected $primaryKey = 'id_user';
+    
     protected $fillable = [
-        'name',
+        'nama_user',
         'email',
         'password',
+        'level',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
